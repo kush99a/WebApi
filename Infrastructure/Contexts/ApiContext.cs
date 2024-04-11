b57a97d378532e1cc3ab655a6b11c124960137ab
kushtrim <kushtrim_shabooni99@hotmail.com>
2024-04-11 14:20:10 +0200

    Add project files.

---
+++ Infrastructure/Contexts/ApiContext.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts;
+
+public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
+{
+    public DbSet<SubscribersEntity> Subscribers { get; set; }
+    public DbSet<CourseEntity> Courses { get; set; }
+}
